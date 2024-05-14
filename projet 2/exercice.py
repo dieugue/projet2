66558d4a08d4930387c3ef637a82ddb32d2948e9
dieugue <dieguek@gmail.com>
2024-05-14 17:20:24 +0100

    vvvv

---
+++ projet 2/exercice.py
@@ -0,0 +1,36 @@
+
+    
+
+#algorithme, qui affiche  20   element en le multipliant  par 7 et que  si le resultat est un multiple de 3 
+# on met un asterisque
+
+n=0
+milt=0
+while n<=20:
+      n=n+1
+      milt=n*7      
+      if milt %3==0:        
+            print(f"7*{n}={milt}*") 
+      else:
+            print(f"7*{n}={milt}")               
+    
+
+#algorithme conversion de fc en dollars   et en euro
+
+fc,dol,eur =0,0.65,1
+while eur<=10:
+      fc=fc+1
+      fc,dol,eur=dol,fc+dol,dol+0.50 
+      print(f"{fc} fc =  {dol} $ = {eur} £")
+
+   
+ #algorithme conversion de fc en dollars   et en euro
+
+n=0
+while n<=12:
+        n=n+1
+        triple=n*3
+        print(f"{n} le triple est {triple}")
+         
+
+
